6d4274bbcd2b27368e982d744e6e9014e05ba29b
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-13 22:14:52 +0530

    Create b_Exercise.py
    Exercise 2: Basic Printing Strings using print function and escape sequences.

---
+++ Python_Programming/Experiments /a_Experiment_1 /b_Exercise.py
@@ -0,0 +1,14 @@
+'''
+Write Python programs to print strings in the given manner: 
+a)   Hello Everyone !!! 
+b)   Hello 
+      World 
+c)   Hello 
+              World 
+d) ‘ Rohit’ s date of birth is 12\05\1999’
+'''
+
+print("Hello Everyone !!!")                            # a) ans
+print("Hello\nWorld")                                  # b) ans
+print("Hello\n\tWorld")                                # c) ans
+print("\'Rohit's date of birth is 12\\05\\1999\'")     # d) ans
