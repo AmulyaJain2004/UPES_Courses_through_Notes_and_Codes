19e4fa96e02439d7bb190a3f7b583f29260f8077
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 23:00:17 +0530

    Create l_Exercise.py
    Program to determine whether the member exist in the list using membership operator.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /l_Exercise.py
@@ -0,0 +1,7 @@
+'''
+Using membership operator find whether a given number is in sequence 
+(10,20,56,78,89) 
+'''
+list=[10,20,56,78,89]
+item = int(input('Enter number to be searched: '))
+print(f"Is the item present? {item in list}")
