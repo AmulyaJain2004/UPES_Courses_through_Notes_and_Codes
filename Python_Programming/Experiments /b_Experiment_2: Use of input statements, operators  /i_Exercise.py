10f44c80795f8480af8497d318385d4907a1b7d7
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 22:55:06 +0530

    Create i_Exercise.py
    Program to calculate the sum of all numbers till n specified by user.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /i_Exercise.py
@@ -0,0 +1,6 @@
+'''
+Write a program to find sum of first n natural numbers.
+'''
+number =  int (input('Enter the number till which sum of all natural numbers has to be done: '))
+summation = (number*(number+1))//2
+print(f"Summation of numbers: {summation}")
