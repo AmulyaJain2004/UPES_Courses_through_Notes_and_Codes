15d9a8ecbe23a5c6ea41cfca8acc811a914d38db
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 22:58:11 +0530

    Create k_Exercise.py
    Program to demonstrate shift operators.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /k_Exercise.py
@@ -0,0 +1,8 @@
+'''
+Write a program to find left shift and right shift values of a given number. 
+'''
+a = int(input())
+b = int(input())
+
+print(f"Right Shift Operator: {a>>b}")
+print(f"Left Shift Operator: {a<<b}")
