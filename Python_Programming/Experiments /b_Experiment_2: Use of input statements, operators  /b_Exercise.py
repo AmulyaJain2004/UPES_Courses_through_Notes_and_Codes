ee417f0b9dc9e2576a935484fd0300b305fc4f41
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:27:11 +0530

    Create b_Exercise.py
    Program about fstrings and printing area of circle by taking inputs.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /b_Exercise.py
@@ -0,0 +1,8 @@
+'''
+Write a Program where the radius is taken as input to compute the area of a 
+circle.
+'''
+radius = float(input('Enter the value of x: '))
+area = float(3.14159*radius*radius)
+
+print(f"Area of circle is: {area}")
