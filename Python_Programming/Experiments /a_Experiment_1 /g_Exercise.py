ce35834459c4ce68c1851b69764cbca1fd7f493b
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:10:30 +0530

    Create g_Exercise.py
    Program about printing string in a speciofic given format.

---
+++ Python_Programming/Experiments /a_Experiment_1 /g_Exercise.py
@@ -0,0 +1,18 @@
+'''
+Declare and assign values to suitable variables and print in the following way : 
+NAME : NIKUNJ BANSAL   
+SAP ID : 500069944 
+DATE OF BIRTH : 13 Oct 1999 
+ADDRESS : UPES 
+                    Bidholi Campus 
+                    Pincode : 248007 
+Programme : AI & ML                                                                                       
+Semester : 2 
+'''
+name=" NAME: NIKUNJ BANSAL"
+sap_id="SAP ID: 500069944"
+dob="DATE OF BIRTH: 13 Oct 1999"
+address="ADDRESS: UPES\n\t  Bidholi\n\t  Campus Pincode : 248007"
+prog="Programme: AI & ML"
+sem="Semester: 2"
+print(name, "\n", sap_id, "\n", dob, "\n", address, "\n", prog, "\n", sem )
