0325625a1b4495c06a1d0f6970b30f7e6128490b
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:24:34 +0530

    Create a_Exercise.py
    Program about different operators in python.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /a_Exercise.py
@@ -0,0 +1,19 @@
+'''
+Declare these variables (x, y and z) as integers. Assign a value of 9 to x, Assign 
+a value of 7 to y, perform addition, multiplication, division and subtraction on 
+these two variables and Print out the result. 
+'''
+x = int(9)
+y = int(7)
+
+z = x+y
+print (z)
+
+z = x*y
+print (z)
+
+z = x//y
+print (f"integer division: {z}")
+
+z = x-y
+print (f"{z}")
