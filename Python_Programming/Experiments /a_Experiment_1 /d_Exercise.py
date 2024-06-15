0baaf5bcbece3be83fb91b34ac4877d22c306313
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:04:59 +0530

    Create d_Exercise.py
    Program about different data types in python

---
+++ Python_Programming/Experiments /a_Experiment_1 /d_Exercise.py
@@ -0,0 +1,12 @@
+'''
+Take different data types and print values using print function. 
+'''
+m=6
+n=8.
+o=987.4579929090
+p=1j
+q=["apple","banana","cherry"]
+r=("apple","banana","cherry")
+s={"Name":"John"}
+t=True
+print(type(m),type(n),type(o),type(p),type(q),type(r),type(s),type(t))
