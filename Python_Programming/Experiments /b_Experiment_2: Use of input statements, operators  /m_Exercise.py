164e9bee93ccd093b182821ffbace35bbb2cc2fa
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 23:02:11 +0530

    Create m_Exercise.py
    Program to check whether a character is present inside a string using membership operator.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /m_Exercise.py
@@ -0,0 +1,6 @@
+'''
+Using membership operator find whether a given character is in a string. 
+'''
+character = input("Enter character to check if it is present or not: ")
+string = "sdjnfsbzdfnksajnfkvjsKJwgduyqwc"
+print(f"Is the character present in string? {character in string}")
