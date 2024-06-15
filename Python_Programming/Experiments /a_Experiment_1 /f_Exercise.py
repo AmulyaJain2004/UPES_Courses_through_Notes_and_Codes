ffe2e1f320a6637a9c5f75bd6737c0aa9b14b1df
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:08:16 +0530

    Create f_Exercise.py
    Program about strings and printing them.

---
+++ Python_Programming/Experiments /a_Experiment_1 /f_Exercise.py
@@ -0,0 +1,10 @@
+'''
+Declare three variables, consisting of your first name, your last name and 
+Nickname. Write a  program that prints out your first name, then your nickname in 
+parenthesis and then your last name.   
+Example output : George ( woody ) Washington.
+'''
+c="Amulya "
+d="Jain"
+e="(Alokik)"
+print(c,e,d)
