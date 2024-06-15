d85ba78eb87fb948726b6012de54af55bb4e1371
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-06-15 18:29:01 +0530

    Create c_Exercise.py
    Program about testing our input and output python program with test inputs.

---
+++ Python_Programming/Experiments /b_Experiment_2: Use of input statements, operators  /c_Exercise.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to solve (x+y)*(x+y) 
+Test data : x = 4 , y = 3 
+Expected output: 49 
+'''
+x = int(input('Enter the value of x: '))
+y = int(input('Enter the value of x: '))
+
+print(f"Value of this expression (x+y)*(x+y) is: {(x+y)*(x+y)}")
