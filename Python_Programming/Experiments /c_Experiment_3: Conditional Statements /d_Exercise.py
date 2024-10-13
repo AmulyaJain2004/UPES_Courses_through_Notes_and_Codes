60d6ae12094c3b34b8fb8cdc611ec14001a49ed4
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-10-13 13:51:08 +0530

    Update d_Exercise.py

--- Python_Programming/Experiments /c_Experiment_3: Conditional Statements /d_Exercise.py
+++ Python_Programming/Experiments /c_Experiment_3: Conditional Statements /d_Exercise.py
@@ -1,1 +1,19 @@
+'''
+Find the greatest among three numbers assuming no two values are same.  
+'''
+num1 = int (input ("Enter number 1: "))
+num2 = int (input ("Enter number 2: "))
+num3 = int (input ("Enter number 3: "))
 
+if ((num1 > num2) and (num2 > num3)):
+    print(num1,"is greater than",num2,"and",num3)
+    
+elif ((num2>num3) and (num2>num1)):
+    print(num2,"is greater than",num1,"and",num3)
+    
+elif ((num3>num1) and (num3>num2)):
+    print(num3,"is greater than",num1,"and",num2)
+    
+elif ((num1==num2) and (num2==num3)):
+    print("All the numbers are equal to each other.")
+  
