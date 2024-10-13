051a1bf714c863cf2e845f5aa5029d4a9c5462d7
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-10-13 13:52:32 +0530

    Update f_Exercise.py

--- Python_Programming/Experiments /c_Experiment_3: Conditional Statements /f_Exercise.py
+++ Python_Programming/Experiments /c_Experiment_3: Conditional Statements /f_Exercise.py
@@ -1,1 +1,21 @@
+'''
+Find whether a given year is a leap year or not. 
+'''
+year = int(input("Enter year to check whether it is leap year or not: "))
+if ((year%4==0 and year%100!=0) or (year%400 == 0)):
+    print("Given year is leap year")
+else:
+    print("Given year is not leap year")
+    
+# if (year%4==0):
+#     if (year%100==0):
+#         if (year%400==0):
+#             print("Given year is leap year")
+#         else:
+#             print("Given year is not leap year")
+#     else:
+#         print("Given year is leap year")
+# else:
+#     print("Given year is not leap year")
 
+# more better
