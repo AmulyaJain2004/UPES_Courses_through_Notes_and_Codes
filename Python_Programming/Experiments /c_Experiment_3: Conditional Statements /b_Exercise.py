9e41829023bdbcb0200e039379076b20c3905d4c
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-10-13 13:49:21 +0530

    Update b_Exercise.py

--- Python_Programming/Experiments /c_Experiment_3: Conditional Statements /b_Exercise.py
+++ Python_Programming/Experiments /c_Experiment_3: Conditional Statements /b_Exercise.py
@@ -1,1 +1,8 @@
-
+'''
+Check whether a given number is multiple of five or not. 
+'''
+number = int(input("Enter the number to check if it is multiple of 5: "))
+if (number%5==0):
+    print("Number is a mutiple of 5.")
+else:
+    print("Number is not multiple of 5.")
