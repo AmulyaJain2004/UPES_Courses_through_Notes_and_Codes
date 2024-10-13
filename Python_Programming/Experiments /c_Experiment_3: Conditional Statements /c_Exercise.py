69adab338f020262b9265c6fbd533427d35d9531
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-10-13 13:50:18 +0530

    Update c_Exercise.py

--- Python_Programming/Experiments /c_Experiment_3: Conditional Statements /c_Exercise.py
+++ Python_Programming/Experiments /c_Experiment_3: Conditional Statements /c_Exercise.py
@@ -1,1 +1,14 @@
+'''
+Find the greatest among two numbers. If numbers are equal than print 
+“numbers are equal”.
+'''
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
 
+if (number1>number2):
+    print("Number:",number1,"is greater than Number:",number2)
+elif (number2>number1):
+    print("Number:",number2,"is greater than Number:",number1)
+else:
+    print("Numbers",number1,"&",number2,"are equal.")
+    
