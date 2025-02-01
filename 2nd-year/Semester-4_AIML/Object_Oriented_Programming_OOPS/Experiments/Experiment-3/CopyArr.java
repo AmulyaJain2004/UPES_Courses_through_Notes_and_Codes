cbe3dc190cb919d97ea63bd1e2e1337a6be4e7a5
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-02-01 23:03:53 +0530

    Added codes

---
+++ 2nd-year/Semester-4_AIML/Object_Oriented_Programming_OOPS/Experiments/Experiment-3/CopyArr.java
@@ -0,0 +1,3 @@
+class CopyArr {
+    public 
+}
--- 2nd-year/Semester-4_AIML/Object_Oriented_Programming_OOPS/Experiments/Experiment-3/pattern.java
+++ 2nd-year/Semester-4_AIML/Object_Oriented_Programming_OOPS/Experiments/Experiment-3/pattern.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 class pattern {
     public static void main(String args[])
     {
-        
+        System.out.println("Amulya Jain 500122439");
+        System.out.println("Enter the number of rows to be printed: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 1; i <= n; i ++){
+            if (i%2 == 0){
+                for (int j = 0; j < 2*i-1; j++){
+                    System.out.print("#");
+                }
+            }
+            else{
+                for (int j = 0; j <= 2*i-2; j++){
+                    System.out.print("?");
+                }
+            }
+            System.out.println();
+        }
     }
 }
